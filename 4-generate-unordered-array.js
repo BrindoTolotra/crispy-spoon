03b70ab7aa3f2f43d0e676b5614419a64ebaf4c8
Brindo Ratiharimanana <98638794+BrindoTolotra@users.noreply.github.com>
2022-04-01 16:00:53 +0200

    Update 4-generate-unordered-array.js

--- 4-generate-unordered-array.js
+++ 4-generate-unordered-array.js
@@ -1,15 +1,21 @@
 const { arrayContain } = require('./3-array-contain');
 
 function generateUnorderedArray(size) {
-  let array = [];
-  for (let i = 0; i < size; i++) {
-    let number = Math.floor(Math.random() * 100);
-    while (arrayContain(array, number)) {
-      number = Math.floor(Math.random() * 100);
+  let array = []; // 1
+  for (let i = 0; i < size; i++) { // 5
+    let number = Math.floor(Math.random() * 100); // 3
+    while (arrayContain(array, number)) { 
+      number = Math.floor(Math.random() * 100); // 3
     }
-    array.push(number);
+    array.push(number); // 2
   }
-  return array;
+  return array; // 1
 }
 
 module.exports = { generateUnorderedArray };
+
+/*
+1 + (5 + 3 + O(n) + 3) * n + 2 + 1 
+O(1) + (O(1) + O(n)) * O(n) + O(1) 
+La compléxité et de O(n) * O(n) donc O(n^2)
+*/
