85d57eeb0797797f1f6de05f54907dd65c9fcfca
Brindo Ratiharimanana <98638794+BrindoTolotra@users.noreply.github.com>
2022-04-01 16:10:53 +0200

    Update-6-main.js

--- 6-main.js
+++ 6-main.js
@@ -3,13 +3,13 @@ const { merge }= require('./2-merge');
 const { generateUnorderedArray } = require('./4-generate-unordered-array');
 
 function sort(array) {
-  if (array.length <= 1) {
-    return array;
+  if (array.length <= 1) { // 2
+    return array; // 1
   }
-  const middle = Math.floor(array.length / 2);
-  const left = slice(array, 0, middle);
-  const right = slice(array, middle, array.length);
-  return merge(sort(left), sort(right));
+  const middle = Math.floor(array.length / 2); // 3
+  const left = slice(array, 0, middle); // 2
+  const right = slice(array, middle, array.length); // 3
+  return merge(sort(left), sort(right));  // 
 }
 
 const array = generateUnorderedArray(10);
