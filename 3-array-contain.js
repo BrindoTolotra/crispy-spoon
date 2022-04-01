5e016748e6e8987d88e8d2e10453983c6228e41b
Brindo Ratiharimanana <98638794+BrindoTolotra@users.noreply.github.com>
2022-04-01 15:58:36 +0200

    Update 3-array-contain.js

--- 3-array-contain.js
+++ 3-array-contain.js
@@ -1,10 +1,17 @@
 function arrayContain(array, number) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === number) {
-      return true;
+  for (let i = 0; i < array.length; i++) { // 5
+    if (array[i] === number) { // 4
+      return true; // 1
     }
   }
-  return false;
+  return false; // 1
 }
 
 module.exports = { arrayContain };
+
+/*
+soit n = array.lenght
+n * (5 + 4 + 1) + 1
+O(n) * O(1) + O(1) 
+comme O(1) << O(n) la compléxité est O(n)
+*/
