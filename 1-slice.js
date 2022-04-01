5546296b2872ddb5f23dee48581ef90537305cf1
Brindo Ratiharimanana <98638794+BrindoTolotra@users.noreply.github.com>
2022-04-01 15:45:07 +0200

    Update 1-slice.js

--- 1-slice.js
+++ 1-slice.js
@@ -1,9 +1,16 @@
 function slice(array, start, end) {
-  let slicedArray = [];
-  for (let i = start; i < end; i++) {
-    slicedArray.push(array[i]);
+  let slicedArray = []; // 1
+  for (let i = start; i < end; i++) { // 6
+    slicedArray.push(array[i]); // 4
   }
-  return slicedArray;
+  return slicedArray; // 1
 }
 
-module.exports = { slice };
+module.exports = { slice }; 
+
+/* comme O(1) << O(n), la compléxité de cette algorithme et O(n)
+      n * (6+4) + 1 
+      => n * (10) + 1
+      => O(n) * (O(1) + (O(1))
+      la compléxité de cette algorithme et O(n)
+*/
